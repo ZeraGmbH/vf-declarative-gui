bea51fcad27417c9db694c5a3be6d3f685336cb6
bhamacher <b.hamacher@zera.de>
2020-07-02 08:24:46 +0200

    Replaced SortFilterProxyModel 3rdParty with SortFilterProxyModelCMake Library .gitmodules: removed module CMakeLists.txt: added dependency include3rdParty.cmake: removed sources
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- 3rdparty/include3rdParty.cmake
+++ 3rdparty/include3rdParty.cmake
@@ -88,19 +88,6 @@ file(GLOB_RECURSE JSONSET_SOURCES
 file(GLOB JSONSET_RESOURCES
 
     )
-##SortFilterProxyModel
-
-file(GLOB SORTF_HEADERS
-    ${PROJECT_SOURCE_DIR}/3rdparty/SortFilterProxyModel/*.h    	
-    )
-
-file(GLOB SORTF_SOURCES
-    ${PROJECT_SOURCE_DIR}/3rdparty/SortFilterProxyModel/*.cpp
-    )
-
-file(GLOB SORTF_RESOURCES
-    ${PROJECT_SOURCE_DIR}/3rdparty/SortFilterProxyModel/*.qrc
-    )
 
 # sum up 3rdParty sources
 set(3RDPARTY_SOURCES   ${QNANO_SOURCES} ${JSONSET_SOURCES} ${SORTF_SOURCES})
