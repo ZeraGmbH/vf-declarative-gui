d14e3d9f719d1c868cbcfdd074018d96d55e3782
Andreas Müller <schnitzeltony@gmail.com>
2022-08-01 17:22:30 +0200

    zeragluelogic: Break unreadable chunk into pieces - start with ActualValueModel
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/zeragluelogic.h
+++ src/zeragluelogic.h
@@ -6,6 +6,20 @@
 #include "gluelogicpropertymap.h"
 class ZeraGlueLogicPrivate;
 
+namespace CommonTable
+{
+enum RoleIndexes
+{
+    NAME=Qt::UserRole,
+    L1,
+    L2,
+    L3,
+    AUX=Qt::UserRole+500,
+    SUM=Qt::UserRole+1000,
+    UNIT=Qt::UserRole+1001,
+};
+}
+
 class ZeraGlueLogic : public VeinEvent::EventSystem
 {
     Q_OBJECT
