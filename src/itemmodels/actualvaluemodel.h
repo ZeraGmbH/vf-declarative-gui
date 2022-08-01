d5a9937c1b0667501a3691c497dbc41d36cef6b0
Andreas Müller <schnitzeltony@gmail.com>
2022-08-01 21:03:50 +0200

    Introduce ZeraGlueLogicItemModelBase::updateTranslation
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/itemmodels/actualvaluemodel.h
+++ src/itemmodels/actualvaluemodel.h
@@ -10,9 +10,15 @@ public:
     ActualValueModel(int t_rows, int t_columns, QObject *t_parent);
     virtual ~ActualValueModel() override;
     void setupTable() override;
-    // QAbstractItemModel interface
-public:
+    void updateTranslation() override;
+    void updateMModeTranslations();
     QHash<int, QByteArray> roleNames() const override;
+
+    // All was mixed up in ZeraGlueLogicPrivate. As long as this mess is
+    // still tightly coupled, give insert access to m_dynamicMeasuringModeDescriptor
+    void insertMeasMode(int yCoordinate, QString measMode);
+private:
+    QHash<int, QString> m_dynamicMeasuringModeDescriptor = {{10, ""}, {11, ""}, {12, ""}};
 };
 
 #endif // ACTUALVALUEMODEL_H
