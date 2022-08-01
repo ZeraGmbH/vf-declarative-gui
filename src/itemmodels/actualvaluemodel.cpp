69350d7d149ea59b90ec1c4434f77dbbc128e206
Andreas Müller <schnitzeltony@gmail.com>
2022-08-01 22:15:54 +0200

    Move value mapping to ZeraGlueLogicItemModelBase
    Implement ActualValueModel as first example
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/itemmodels/actualvaluemodel.cpp
+++ src/itemmodels/actualvaluemodel.cpp
@@ -7,6 +7,9 @@ ActualValueModel::ActualValueModel(int t_rows, int t_columns, QObject *t_parent)
 
 ActualValueModel::~ActualValueModel()
 {
+    for(auto point : qAsConst(m_valueMapping)) {
+        delete point;
+    }
 }
 
 void ActualValueModel::setupTable()
@@ -77,6 +80,86 @@ void ActualValueModel::setupTable()
     mIndex = index(13, 0);
     setData(mIndex, "Hz", RoleIndexes::UNIT);
 }
+
+void ActualValueModel::setupMapping()
+{
+    using namespace CommonTable;
+    QHash<QString, QPoint> *rmsMap = new QHash<QString, QPoint>();
+    rmsMap->insert("ACT_RMSPN1", QPoint(RoleIndexes::L1, 1));
+    rmsMap->insert("ACT_RMSPN2", QPoint(RoleIndexes::L2, 1));
+    rmsMap->insert("ACT_RMSPN3", QPoint(RoleIndexes::L3, 1));
+    rmsMap->insert("ACT_RMSPN7", QPoint(RoleIndexes::AUX, 1));
+
+    rmsMap->insert("ACT_RMSPP1", QPoint(RoleIndexes::L1, 2));
+    rmsMap->insert("ACT_RMSPP2", QPoint(RoleIndexes::L2, 2));
+    rmsMap->insert("ACT_RMSPP3", QPoint(RoleIndexes::L3, 2));
+
+    QHash<QString, QPoint> *dftMap = new QHash<QString, QPoint>();
+    dftMap->insert("ACT_DFTPN1", QPoint(RoleIndexes::L1, 3));
+    dftMap->insert("ACT_DFTPN2", QPoint(RoleIndexes::L2, 3));
+    dftMap->insert("ACT_DFTPN3", QPoint(RoleIndexes::L3, 3));
+    dftMap->insert("ACT_DFTPN7", QPoint(RoleIndexes::AUX, 3));
+
+    QHash<QString, QPoint> *thdnMap = new QHash<QString, QPoint>();
+    thdnMap->insert("ACT_THDR1", QPoint(RoleIndexes::L1, 4));
+    thdnMap->insert("ACT_THDR2", QPoint(RoleIndexes::L2, 4));
+    thdnMap->insert("ACT_THDR3", QPoint(RoleIndexes::L3, 4));
+
+    rmsMap->insert("ACT_RMSPN4", QPoint(RoleIndexes::L1, 5));
+    rmsMap->insert("ACT_RMSPN5", QPoint(RoleIndexes::L2, 5));
+    rmsMap->insert("ACT_RMSPN6", QPoint(RoleIndexes::L3, 5));
+    rmsMap->insert("ACT_RMSPN8", QPoint(RoleIndexes::AUX, 5));
+
+    dftMap->insert("ACT_DFTPN4", QPoint(RoleIndexes::L1, 6));
+    dftMap->insert("ACT_DFTPN5", QPoint(RoleIndexes::L2, 6));
+    dftMap->insert("ACT_DFTPN6", QPoint(RoleIndexes::L3, 6));
+    dftMap->insert("ACT_DFTPN8", QPoint(RoleIndexes::AUX, 6));
+
+    thdnMap->insert("ACT_THDR4", QPoint(RoleIndexes::L1, 7));
+    thdnMap->insert("ACT_THDR5", QPoint(RoleIndexes::L2, 7));
+    thdnMap->insert("ACT_THDR6", QPoint(RoleIndexes::L3, 7));
+
+    //(8) ∠UI is a calculated value
+
+    QHash<QString, QPoint> *lambdaMap = new QHash<QString, QPoint>();
+    lambdaMap->insert("ACT_Lambda1", QPoint(RoleIndexes::L1, 9));
+    lambdaMap->insert("ACT_Lambda2", QPoint(RoleIndexes::L2, 9));
+    lambdaMap->insert("ACT_Lambda3", QPoint(RoleIndexes::L3, 9));
+    lambdaMap->insert("ACT_Lambda4", QPoint(RoleIndexes::SUM, 9));
+
+    QHash<QString, QPoint> *p1m1Map = new QHash<QString, QPoint>();
+    p1m1Map->insert("PAR_MeasuringMode", QPoint(RoleIndexes::NAME, 10));
+    p1m1Map->insert("ACT_PQS1", QPoint(RoleIndexes::L1, 10));
+    p1m1Map->insert("ACT_PQS2", QPoint(RoleIndexes::L2, 10));
+    p1m1Map->insert("ACT_PQS3", QPoint(RoleIndexes::L3, 10));
+    p1m1Map->insert("ACT_PQS4", QPoint(RoleIndexes::SUM, 10));
+
+    QHash<QString, QPoint> *p1m2Map = new QHash<QString, QPoint>();
+    p1m2Map->insert("PAR_MeasuringMode", QPoint(RoleIndexes::NAME, 11));
+    p1m2Map->insert("ACT_PQS1", QPoint(RoleIndexes::L1, 11));
+    p1m2Map->insert("ACT_PQS2", QPoint(RoleIndexes::L2, 11));
+    p1m2Map->insert("ACT_PQS3", QPoint(RoleIndexes::L3, 11));
+    p1m2Map->insert("ACT_PQS4", QPoint(RoleIndexes::SUM, 11));
+
+    QHash<QString, QPoint> *p1m3Map = new QHash<QString, QPoint>();
+    p1m3Map->insert("PAR_MeasuringMode", QPoint(RoleIndexes::NAME, 12));
+    p1m3Map->insert("ACT_PQS1", QPoint(RoleIndexes::L1, 12));
+    p1m3Map->insert("ACT_PQS2", QPoint(RoleIndexes::L2, 12));
+    p1m3Map->insert("ACT_PQS3", QPoint(RoleIndexes::L3, 12));
+    p1m3Map->insert("ACT_PQS4", QPoint(RoleIndexes::SUM, 12));
+
+    QHash<QString, QPoint> *rangeMap = new QHash<QString, QPoint>();
+    rangeMap->insert("ACT_Frequency", QPoint(RoleIndexes::SUM, 13));
+
+
+    m_valueMapping.insert(static_cast<int>(Modules::RmsModule), rmsMap);
+    m_valueMapping.insert(static_cast<int>(Modules::ThdnModule2), thdnMap);
+    m_valueMapping.insert(static_cast<int>(Modules::DftModule), dftMap);
+    m_valueMapping.insert(static_cast<int>(Modules::LambdaModule), lambdaMap);
+    m_valueMapping.insert(static_cast<int>(Modules::Power1Module1), p1m1Map);
+    m_valueMapping.insert(static_cast<int>(Modules::Power1Module2), p1m2Map);
+    m_valueMapping.insert(static_cast<int>(Modules::Power1Module3), p1m3Map);
+    m_valueMapping.insert(static_cast<int>(Modules::RangeModule), rangeMap);}
 
 void ActualValueModel::updateTranslation()
 {
