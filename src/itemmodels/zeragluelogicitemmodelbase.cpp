fd88ce0041057c686ea8c4eeadac51ced5fdcaa9
Andreas Müller <schnitzeltony@gmail.com>
2022-08-10 18:03:41 +0200

    ZeraGlueLogicItemModelBase: Add getter to all objects of this class
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/itemmodels/zeragluelogicitemmodelbase.cpp
+++ src/itemmodels/zeragluelogicitemmodelbase.cpp
@@ -1,9 +1,12 @@
 #include "zeragluelogicitemmodelbase.h"
 
+QSet<ZeraGlueLogicItemModelBase*> ZeraGlueLogicItemModelBase::m_setAllBaseModels;
+
 ZeraGlueLogicItemModelBase::ZeraGlueLogicItemModelBase(int t_rows, int t_columns, QObject *t_parent) :
     QStandardItemModel(t_rows, t_columns, t_parent),
     m_translation(ZeraTranslation::getInstance())
 {
+    m_setAllBaseModels.insert(this);
 }
 
 ZeraGlueLogicItemModelBase::~ZeraGlueLogicItemModelBase()
@@ -11,6 +14,12 @@ ZeraGlueLogicItemModelBase::~ZeraGlueLogicItemModelBase()
     for(auto point : qAsConst(m_valueMapping)) {
         delete point;
     }
+    m_setAllBaseModels.remove(this);
+}
+
+QList<ZeraGlueLogicItemModelBase *> ZeraGlueLogicItemModelBase::getAllBaseModels()
+{
+    return m_setAllBaseModels.values();
 }
 
 QHash<int, QHash<QString, QPoint> *> ZeraGlueLogicItemModelBase::getValueMapping()
