2b377c4d23a861ece0fa00412585e2affaec0392
Andreas Müller <schnitzeltony@gmail.com>
2022-09-12 18:35:29 +0200

    Write tests for VfEventDispatcher
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- tests/veinconsumermock.cpp
+++ tests/veinconsumermock.cpp
@@ -2,5 +2,14 @@
 
 void VeinConsumerMock::handleComponentChange(const VeinComponent::ComponentData *cData)
 {
+    TComponentInfo componentInfo;
+    componentInfo.componentName = cData->componentName();
+    componentInfo.entityId = cData->entityId();
+    componentInfo.value = cData->newValue();
+    m_componentChangeList.append(componentInfo);
+}
 
+QList<VeinConsumerMock::TComponentInfo> VeinConsumerMock::getComponentChangeList()
+{
+    return m_componentChangeList;
 }
--- tests/veinconsumermock.h
+++ tests/veinconsumermock.h
@@ -5,10 +5,18 @@
 
 
 class VeinConsumerMock : public VfEventConsumerInterface
-
 {
 public:
     void handleComponentChange(const VeinComponent::ComponentData *cData) override;
+    struct TComponentInfo
+    {
+        int entityId = -1;
+        QString componentName;
+        QVariant value;
+    };
+    QList<TComponentInfo> getComponentChangeList();
+private:
+    QList<TComponentInfo> m_componentChangeList;
 };
 
 #endif // VEINCONSUMERMOCK_H
