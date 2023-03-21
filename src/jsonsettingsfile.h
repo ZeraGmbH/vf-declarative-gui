8bce7f4d0fa6820c5f412ac491c66e5135b128f9
Andreas Müller <schnitzeltony@gmail.com>
2023-03-22 00:13:55 +0100

    jsonsettingsfile: Get rid of over-enthusiastic but never used ideas
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/jsonsettingsfile.h
+++ src/jsonsettingsfile.h
@@ -11,33 +11,21 @@ class JsonSettingsFile : public QObject
 public:
     explicit JsonSettingsFile(QObject *t_parent = nullptr);
     static JsonSettingsFile *getInstance();
-    static JsonSettingsFile *getStaticInstance(QQmlEngine *t_engine=nullptr, QJSEngine *t_scriptEngine=nullptr);
+    static JsonSettingsFile *getStaticInstance(QQmlEngine *t_engine, QJSEngine *t_scriptEngine);
+    bool loadFromStandardLocation(const QString &t_fileName);
+    void setAutoWriteBackEnabled(bool t_autoWriteBackEnabled=true);
 
-    Q_INVOKABLE bool fileExists(const QString &t_filePath) const;
-    Q_INVOKABLE void reloadFile();
-    Q_INVOKABLE bool loadFromStandardLocation(const QString &t_fileName);
-    Q_INVOKABLE bool loadFromFile(const QString &t_filePath);
-    Q_INVOKABLE void saveToFile(const QString &t_filePath, bool t_overwrite=false);
-    Q_INVOKABLE QString getCurrentFilePath();
-
-    Q_INVOKABLE bool hasOption(const QString &t_key);
     Q_INVOKABLE QString getOption(const QString &t_key, const QString &t_valueDefault);
     Q_INVOKABLE bool setOption(const QString &t_key, const QString &t_value);
-    Q_INVOKABLE bool dropOption(const QString &t_key);
-    Q_PROPERTY(bool fireChangeOnSet READ fireChangeOnSet WRITE setFireChangeOnSet NOTIFY fireChangeOnSetChanged)
-
-    Q_INVOKABLE bool autoWriteBackEnabled() const;
-    Q_INVOKABLE void setAutoWriteBackEnabled(bool t_autoWriteBackEnabled=true);
 signals:
     void settingsChanged(JsonSettingsFile *settingsFile);
     void settingsSaveRequest(JsonSettingsFile *settingsFile);
-    void fireChangeOnSetChanged();
-
-public slots:
 
 private:
-    bool fireChangeOnSet() const;
-    void setFireChangeOnSet(bool enabled);
+    bool loadFromFile(const QString &t_filePath);
+    void saveToFile(const QString &t_filePath, bool t_overwrite=false);
+    QString getCurrentFilePath();
+    bool hasOption(const QString &t_key);
 
     JsonSettingsFilePrivate *d_ptr;
 
