119272dce434ec507e2e08868ffc03842ba5c756
Asmae Kaouissi <a.kaouissi@zera.de>
2023-06-05 14:22:11 +0200

    actualvaluemodel: change power unit on new measurement mode
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- src/itemmodels/actualvaluemodel.h
+++ src/itemmodels/actualvaluemodel.h
@@ -21,9 +21,11 @@ protected:
 private:
     void insertMeasMode(int yCoordinate, QString measMode);
     void insertPowerName(int yCoordinate, QString measMode);
+    void insertPowerUnit(int yCoordinate, QString measUnit);
     void updateMModeTranslations();
     QHash<int, QString> m_dynamicMeasuringModeDescriptor = {{10, ""}, {11, ""}, {12, ""}};
     QHash<int, QString> m_dynamicPowerName = {{10, ""}, {11, ""}, {12, ""}};
+    QHash<int, QString> m_dynamicPowerUnit = {{10, ""}, {11, ""}, {12, ""}};
 };
 
 #endif // ACTUALVALUEMODEL_H
