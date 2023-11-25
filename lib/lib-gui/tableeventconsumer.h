c508e8b7b9dc25320608e144d0714543111bfc04
Andreas Müller <schnitzeltony@gmail.com>
2023-11-25 19:02:54 +0100

    tableeventconsumer: Remove unneeded burden extra handling
    handleBurdenValues adds nothing and is done already by initial loop in
    TableEventConsumer::handleComponentChange
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- lib/lib-gui/tableeventconsumer.h
+++ lib/lib-gui/tableeventconsumer.h
@@ -24,7 +24,6 @@ private:
     void setAngleUI(int t_systemNumber);
 
     bool handleActualValues(TableEventItemModelBase *itemModel, QHash<QString, QPoint>* t_componentMapping, const VeinComponent::ComponentData *t_cmpData);
-    bool handleBurdenValues(TableEventItemModelBase *itemModel, QHash<QString, QPoint>* t_componentMapping, const VeinComponent::ComponentData *t_cmpData);
     bool handleFftValues(const VeinComponent::ComponentData *t_cmpData);
     bool handleHarmonicPowerValues(const VeinComponent::ComponentData *t_cmpData);
 
