433910144e202474f5013d676230b5ec44827931
i-sundermann <i.sundermann@zera.de>
2024-04-03 11:19:47 +0200

    qmlfileio: change indentation (from 2 to 4)
    no functional change
    Signed-off-by: i-sundermann <i.sundermann@zera.de>

--- lib/lib-gui/qmlfileio.cpp
+++ lib/lib-gui/qmlfileio.cpp
@@ -15,13 +15,13 @@
 
 namespace QmlFileIOPrivate
 {
-  void registerTypes()
-  {
+void registerTypes()
+{
     // @uri QmlFileIO
     qmlRegisterSingletonType<QmlFileIO>("QmlFileIO", 1, 0, "QmlFileIO", QmlFileIO::getStaticInstance);
-  }
+}
 
-  Q_COREAPP_STARTUP_FUNCTION(registerTypes)
+Q_COREAPP_STARTUP_FUNCTION(registerTypes)
 }
 
 QmlFileIO::QmlFileIO(QObject *t_parent) : QObject(t_parent)
@@ -33,170 +33,170 @@ QmlFileIO::QmlFileIO(QObject *t_parent) : QObject(t_parent)
 
 QString QmlFileIO::readTextFile(const QString &t_fileName)
 {
-  QFile textFile(t_fileName);
-  QString retVal;
-  if(checkFile(textFile) && textFile.open(QFile::ReadOnly | QFile::Text))
-  {
-    QTextStream textStream(&textFile);
-    retVal = textStream.readAll();
-    textFile.close();
-  }
-  return retVal;
+    QFile textFile(t_fileName);
+    QString retVal;
+    if(checkFile(textFile) && textFile.open(QFile::ReadOnly | QFile::Text))
+    {
+        QTextStream textStream(&textFile);
+        retVal = textStream.readAll();
+        textFile.close();
+    }
+    return retVal;
 }
 
 bool QmlFileIO::writeTextFile(const QString &t_fileName, const QString &t_content, bool t_overwrite, bool t_truncate)
 {
-  QFile textFile(t_fileName);
-  bool retVal = false;
-  if(textFile.exists() == false || t_overwrite == true)
-  {
-    bool fileIsOpen = false;
-    if(t_truncate == true)
+    QFile textFile(t_fileName);
+    bool retVal = false;
+    if(textFile.exists() == false || t_overwrite == true)
     {
-      fileIsOpen = textFile.open(QFile::WriteOnly);
+        bool fileIsOpen = false;
+        if(t_truncate == true)
+        {
+            fileIsOpen = textFile.open(QFile::WriteOnly);
+        }
+        else
+        {
+            fileIsOpen = textFile.open(QFile::Append);
+        }
+
+        if(fileIsOpen == true)
+        {
+            retVal = true;
+            QTextStream textStream(&textFile);
+            textStream << t_content;
+            textFile.close();
+        }
+        else
+        {
+            qWarning() << "QmlFileIO: Error opening file:" << t_fileName << "error:" << textFile.errorString();
+        }
     }
     else
     {
-      fileIsOpen = textFile.open(QFile::Append);
+        qWarning() << "QmlFileIO: Skipped writing existing file because of missing override flag" << t_fileName;
     }
 
-    if(fileIsOpen == true)
-    {
-      retVal = true;
-      QTextStream textStream(&textFile);
-      textStream << t_content;
-      textFile.close();
-    }
-    else
-    {
-      qWarning() << "QmlFileIO: Error opening file:" << t_fileName << "error:" << textFile.errorString();
-    }
-  }
-  else
-  {
-    qWarning() << "QmlFileIO: Skipped writing existing file because of missing override flag" << t_fileName;
-  }
-
-  return retVal;
+    return retVal;
 }
 
 QVariant QmlFileIO::readJsonFile(const QString &t_fileName)
 {
-  QFile jsonFile(t_fileName);
-  QVariant retVal;
-  if(checkFile(jsonFile))
-  {
-    if(jsonFile.open(QFile::ReadOnly))
+    QFile jsonFile(t_fileName);
+    QVariant retVal;
+    if(checkFile(jsonFile))
     {
-      QJsonParseError errorObj;
-      QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonFile.readAll(), &errorObj);
-      if(errorObj.error == QJsonParseError::NoError)
-      {
-        if(jsonDoc.isObject())
+        if(jsonFile.open(QFile::ReadOnly))
         {
-          retVal = jsonDoc.object().toVariantMap();
+            QJsonParseError errorObj;
+            QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonFile.readAll(), &errorObj);
+            if(errorObj.error == QJsonParseError::NoError)
+            {
+                if(jsonDoc.isObject())
+                {
+                    retVal = jsonDoc.object().toVariantMap();
+                }
+                else if(jsonDoc.isArray())
+                {
+                    retVal = jsonDoc.array().toVariantList();
+                }
+            }
+            else
+            {
+                qWarning() << "QmlFileIO: Error parsing JSON file:" << t_fileName << "error:" << errorObj.errorString();
+            }
         }
-        else if(jsonDoc.isArray())
+        else
         {
-          retVal = jsonDoc.array().toVariantList();
+            qWarning() << "QmlFileIO: Error opening file:" << t_fileName << "error:" << jsonFile.errorString();
         }
-      }
-      else
-      {
-        qWarning() << "QmlFileIO: Error parsing JSON file:" << t_fileName << "error:" << errorObj.errorString();
-      }
     }
-    else
-    {
-      qWarning() << "QmlFileIO: Error opening file:" << t_fileName << "error:" << jsonFile.errorString();
-    }
-  }
 
-  return retVal;
+    return retVal;
 }
 
 bool QmlFileIO::writeJsonFile(const QString &t_fileName, const QVariant &t_content, bool t_overwrite)
 {
-  bool retVal = false;
-  QFile jsonFile(t_fileName);
-  QJsonDocument jsonDoc;
-  if(jsonFile.exists() == false || t_overwrite == true)
-  {
-    bool dataIsValid = false;
+    bool retVal = false;
+    QFile jsonFile(t_fileName);
+    QJsonDocument jsonDoc;
+    if(jsonFile.exists() == false || t_overwrite == true)
+    {
+        bool dataIsValid = false;
 
-    if(t_content.type() == QVariant::Map)
-    {
-      QJsonObject jsonObj = QJsonObject::fromVariantMap(t_content.toMap());
-      jsonDoc.setObject(jsonObj);
-      dataIsValid = true;
-    }
-    else if(t_content.type() == QVariant::List)
-    {
-      QJsonArray jsonArray = QJsonArray::fromVariantList(t_content.toList());
-      jsonDoc.setArray(jsonArray);
-      dataIsValid = true;
+        if(t_content.type() == QVariant::Map)
+        {
+            QJsonObject jsonObj = QJsonObject::fromVariantMap(t_content.toMap());
+            jsonDoc.setObject(jsonObj);
+            dataIsValid = true;
+        }
+        else if(t_content.type() == QVariant::List)
+        {
+            QJsonArray jsonArray = QJsonArray::fromVariantList(t_content.toList());
+            jsonDoc.setArray(jsonArray);
+            dataIsValid = true;
+        }
+        else
+        {
+            qWarning() << "QmlFileIO: Expected list or object type to write JSON document:" << t_fileName << "instead provided type is:" << t_content.typeName();
+        }
+
+        if(jsonFile.open(QFile::WriteOnly) && dataIsValid == true)
+        {
+            jsonFile.write(jsonDoc.toJson(QJsonDocument::Indented));
+            jsonFile.close();
+            retVal = true;
+        }
+        else
+        {
+            qWarning() << "QmlFileIO: Error opening file:" << t_fileName << "error:" << jsonFile.errorString();
+        }
     }
     else
     {
-      qWarning() << "QmlFileIO: Expected list or object type to write JSON document:" << t_fileName << "instead provided type is:" << t_content.typeName();
+        qWarning() << "QmlFileIO: Skipped writing existing file because of missing override flag" << t_fileName;
     }
-
-    if(jsonFile.open(QFile::WriteOnly) && dataIsValid == true)
-    {
-      jsonFile.write(jsonDoc.toJson(QJsonDocument::Indented));
-      jsonFile.close();
-      retVal = true;
-    }
-    else
-    {
-      qWarning() << "QmlFileIO: Error opening file:" << t_fileName << "error:" << jsonFile.errorString();
-    }
-  }
-  else
-  {
-    qWarning() << "QmlFileIO: Skipped writing existing file because of missing override flag" << t_fileName;
-  }
-  return retVal;
+    return retVal;
 }
 
 QObject *QmlFileIO::getStaticInstance(QQmlEngine *t_engine, QJSEngine *t_scriptEngine)
 {
-  Q_UNUSED(t_engine)
-  Q_UNUSED(t_scriptEngine)
+    Q_UNUSED(t_engine)
+    Q_UNUSED(t_scriptEngine)
 
-  return s_instance;
+    return s_instance;
 }
 
 void QmlFileIO::setStaticInstance(QmlFileIO *t_instance)
 {
-  if(s_instance == nullptr)
-  {
-    s_instance = t_instance;
-  }
+    if(s_instance == nullptr)
+    {
+        s_instance = t_instance;
+    }
 }
 
 bool QmlFileIO::checkFile(const QFile &t_file)
 {
-  bool retVal = false;
-  if(t_file.exists())
-  {
-    QFileInfo fInfo(t_file);
-    const qint64 fileSize = fInfo.size();
-    if(fileSize > 0 && fileSize < UINT32_MAX) //sanity check
+    bool retVal = false;
+    if(t_file.exists())
     {
-      retVal = true;
+        QFileInfo fInfo(t_file);
+        const qint64 fileSize = fInfo.size();
+        if(fileSize > 0 && fileSize < UINT32_MAX) //sanity check
+        {
+            retVal = true;
+        }
+        else
+        {
+            qWarning() << "QmlFileIO: Only files with 0 < size < 4GB are supported:" << t_file.fileName() << "has:" << fileSize << "bytes";
+        }
     }
     else
     {
-      qWarning() << "QmlFileIO: Only files with 0 < size < 4GB are supported:" << t_file.fileName() << "has:" << fileSize << "bytes";
+        qWarning() << "QmlFileIO: File can not be read:" << t_file.fileName();
     }
-  }
-  else
-  {
-    qWarning() << "QmlFileIO: File can not be read:" << t_file.fileName();
-  }
 
-  return retVal;
+    return retVal;
 }
 
 const QStringList &QmlFileIO::mountedPaths() const
--- lib/lib-gui/qmlfileio.h
+++ lib/lib-gui/qmlfileio.h
@@ -12,36 +12,36 @@
  */
 class QmlFileIO : public QObject
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  QmlFileIO(QObject *t_parent=0);
+    QmlFileIO(QObject *t_parent=0);
 
-  Q_PROPERTY(QStringList mountedPaths READ mountedPaths NOTIFY sigMountedPathsChanged);
+    Q_PROPERTY(QStringList mountedPaths READ mountedPaths NOTIFY sigMountedPathsChanged);
 
-  Q_INVOKABLE QString readTextFile(const QString& t_fileName);
-  Q_INVOKABLE bool writeTextFile(const QString& t_fileName, const QString &t_content, bool t_overwrite = false, bool t_truncate = true);
-  Q_INVOKABLE QVariant readJsonFile(const QString& t_fileName);
-  Q_INVOKABLE bool writeJsonFile(const QString& t_fileName, const QVariant &t_content, bool t_overwrite = false);
-  Q_INVOKABLE bool storeJournalctlOnUsb();
+    Q_INVOKABLE QString readTextFile(const QString& t_fileName);
+    Q_INVOKABLE bool writeTextFile(const QString& t_fileName, const QString &t_content, bool t_overwrite = false, bool t_truncate = true);
+    Q_INVOKABLE QVariant readJsonFile(const QString& t_fileName);
+    Q_INVOKABLE bool writeJsonFile(const QString& t_fileName, const QVariant &t_content, bool t_overwrite = false);
+    Q_INVOKABLE bool storeJournalctlOnUsb();
 
-  static QObject *getStaticInstance(QQmlEngine *t_engine, QJSEngine *t_scriptEngine);
+    static QObject *getStaticInstance(QQmlEngine *t_engine, QJSEngine *t_scriptEngine);
 
-  static void setStaticInstance(QmlFileIO *t_instance);
+    static void setStaticInstance(QmlFileIO *t_instance);
 
-  const QStringList &mountedPaths() const;
+    const QStringList &mountedPaths() const;
 
 signals:
-  void sigMountedPathsChanged();
+    void sigMountedPathsChanged();
 
 private slots:
-  void onMountPathsChanged(QStringList mountPaths);
+    void onMountPathsChanged(QStringList mountPaths);
 
 private:
-  bool checkFile(const QFile &t_file);
+    bool checkFile(const QFile &t_file);
 
-  vfFiles::MountWatcherEntryBase m_mountWatcher;
-  QStringList m_mountedPaths;
-  static QmlFileIO *s_instance;
+    vfFiles::MountWatcherEntryBase m_mountWatcher;
+    QStringList m_mountedPaths;
+    static QmlFileIO *s_instance;
 };
 
 #endif // QMLFILEIO_H
