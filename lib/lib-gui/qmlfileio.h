c1ce4840403bd64a9c19f7764ddaaca9cb67f437
i-sundermann <i.sundermann@zera.de>
2024-04-03 10:40:50 +0200

    Introduce button to store journal to USB stick
    Signed-off-by: i-sundermann <i.sundermann@zera.de>

--- lib/lib-gui/qmlfileio.h
+++ lib/lib-gui/qmlfileio.h
@@ -1,6 +1,7 @@
 #ifndef QMLFILEIO_H
 #define QMLFILEIO_H
 
+#include "mountwatcherentrybase.h"
 #include <QObject>
 #include <QVariant>
 #include <QString>
@@ -15,22 +16,31 @@ class QmlFileIO : public QObject
 public:
   QmlFileIO(QObject *t_parent=0);
 
+  Q_PROPERTY(QStringList mountedPaths READ mountedPaths NOTIFY sigMountedPathsChanged);
+
   Q_INVOKABLE QString readTextFile(const QString& t_fileName);
   Q_INVOKABLE bool writeTextFile(const QString& t_fileName, const QString &t_content, bool t_overwrite = false, bool t_truncate = true);
   Q_INVOKABLE QVariant readJsonFile(const QString& t_fileName);
   Q_INVOKABLE bool writeJsonFile(const QString& t_fileName, const QVariant &t_content, bool t_overwrite = false);
+  Q_INVOKABLE bool storeJournalctlOnUsb();
 
   static QObject *getStaticInstance(QQmlEngine *t_engine, QJSEngine *t_scriptEngine);
 
   static void setStaticInstance(QmlFileIO *t_instance);
 
-signals:
+  const QStringList &mountedPaths() const;
 
-public slots:
+signals:
+  void sigMountedPathsChanged();
+
+private slots:
+  void onMountPathsChanged(QStringList mountPaths);
 
 private:
   bool checkFile(const QFile &t_file);
 
+  vfFiles::MountWatcherEntryBase m_mountWatcher;
+  QStringList m_mountedPaths;
   static QmlFileIO *s_instance;
 };
 
