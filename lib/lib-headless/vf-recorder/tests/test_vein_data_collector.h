3ab4f7b0959e2f7bcd942a2eafe66baa97efb640
amutalik <a.mutalik@zera.de>
2024-11-15 20:00:51 +0100

    test_vein_data_collector: More tests
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- lib/lib-headless/vf-recorder/tests/test_vein_data_collector.h
+++ lib/lib-headless/vf-recorder/tests/test_vein_data_collector.h
@@ -1,6 +1,7 @@
 #ifndef TEST_VEIN_DATA_COLLECTOR_H
 #define TEST_VEIN_DATA_COLLECTOR_H
 
+#include "veindatacollector.h"
 #include <testveinserver.h>
 #include <memory>
 
@@ -11,11 +12,18 @@ private slots:
     void initTestCase();
     void init();
     void cleanup();
-    void oneChangeWithinOnePeriod();
 
+    void oneTimestampOneEntityOneComponentChange();
+    void oneTimestampOneEntityOneComponentChangesTwice();
+    void twoTimestampsOneEntityOneComponentChange();
+    void oneTimestampTwoEntitiesOneComponentChange();
+    void twoTimestampsTwoEntitiesOneComponentChange();
 private:
     void setupServer();
     std::unique_ptr<TestVeinServer> m_server;
+    VeinStorage::TimeStamperSettablePtr m_timeStamper;
+    std::unique_ptr<VeinDataCollector> m_dataCollector;
+    QHash<int, QStringList> m_collectorComponents;
 };
 
 #endif // TEST_VEIN_DATA_COLLECTOR_H
