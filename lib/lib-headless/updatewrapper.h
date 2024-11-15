dd85701104ed34a8c13252fe6cff0aaea55bca95
mdoerr2s <m.doerr@zera.de>
2024-11-15 10:56:51 +0100

    updatewrapper.h: handle errors and stop installation process
    Signed-off-by: mdoerr2s <m.doerr@zera.de>

--- lib/lib-headless/updatewrapper.h
+++ lib/lib-headless/updatewrapper.h
@@ -8,15 +8,24 @@ class UpdateWrapper : public QObject
 {
     Q_OBJECT
 public:
+    Q_PROPERTY(bool updateOk READ getUpdateOk NOTIFY sigUpdateOkChanged);
+    Q_PROPERTY(QString updateMessage READ getUpdateMessage NOTIFY sigUpdateMessageChanged);
     Q_INVOKABLE void startInstallation();
     QString searchForPackages(QString mountPath);
     QStringList getOrderedPackageList(QString zupLocation);
+    bool getUpdateOk() const;
+    QString getUpdateMessage() const;
 private:
     QString m_pathToZups;
     QStringList m_zupsToBeInstalled;
     std::unique_ptr<TaskContainerInterface> m_tasks;
+    bool m_updateOk;
+    QString m_updateMessage;
 signals:
-
+    void sigUpdateOkChanged();
+    void sigUpdateMessageChanged();
+private slots:
+    void onTaskFinished(bool ok, int taskId);
 };
 
 #endif // UPDATEWRAPPER_H
