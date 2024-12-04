c32fbfb664f3dc6a0ed049302fe97cacb7fbc4bd
amutalik <a.mutalik@zera.de>
2024-12-04 10:55:22 +0100

    veindatacollector: Remove old way of recording (vs_storagefilter isn't used anymore)
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- lib/lib-headless/vf-recorder/veindatacollector.cpp
+++ lib/lib-headless/vf-recorder/veindatacollector.cpp
@@ -10,28 +10,15 @@
 static constexpr int dftEntityId = 1050;
 
  VeinDataCollector::VeinDataCollector(VeinStorage::AbstractEventSystem *storage, VeinStorage::TimeStamperSettablePtr timeSetter) :
-    m_storageFilter(storage, VeinStorage::StorageFilter::Settings(false, true)),
     m_timeStamper(timeSetter),
     m_storage(storage)
 {
-    connect(&m_storageFilter, &VeinStorage::StorageFilter::sigComponentValue, this, &VeinDataCollector::appendValue);
-    m_periodicTimer = TimerFactoryQt::createPeriodic(500);
-    connect(m_periodicTimer.get(), &TimerTemplateQt::sigExpired, this, &VeinDataCollector::TimerExpired);
 }
 
 void VeinDataCollector::startLogging(QHash<int, QStringList> entitesAndComponents)
 {
-    m_jsonObject = QJsonObject();
-    m_lastJsonObject = QJsonObject();
     m_completeJson = QJsonObject();
     m_recentJsonObject = QJsonObject();
-    m_currentTimestampRecord.clear();
-    for(auto iter=entitesAndComponents.cbegin(); iter!=entitesAndComponents.cend(); ++iter) {
-        const QStringList components = iter.value();
-        int entityId = iter.key();
-        for(const QString& componentName : components)
-            m_storageFilter.add(entityId, componentName);
-    }
     m_targetEntityComponents = entitesAndComponents;
     prepareTimeRecording();
     qInfo("VeinDataCollector started logging.");
@@ -39,20 +26,8 @@ void VeinDataCollector::startLogging(QHash<int, QStringList> entitesAndComponent
 
 void VeinDataCollector::stopLogging()
 {
-    m_periodicTimer->stop();
-    m_storageFilter.clear();
     disconnect(m_sigMeasuringCompo.get(), 0, this, 0);
     qInfo("VeinDataCollector stopped logging.");
-}
-
-QJsonObject VeinDataCollector::getAllStoredValues()
-{
-    return m_jsonObject;
-}
-
-QJsonObject VeinDataCollector::getLastStoredValues()
-{
-    return m_lastJsonObject;
 }
 
 QJsonObject VeinDataCollector::getCompleteJson()
@@ -67,7 +42,6 @@ QJsonObject VeinDataCollector::getRecentJsonObject()
 
 void VeinDataCollector::clearJson()
 {
-    m_jsonObject = QJsonObject();
     m_completeJson = QJsonObject();
 }
 
@@ -87,44 +61,6 @@ void VeinDataCollector::collectValues(QDateTime timeStamp)
     emit newValueCollected();
 }
 
-void VeinDataCollector::appendValue(int entityId, QString componentName, QVariant value, QDateTime timeStamp)
-{
-    Q_UNUSED(timeStamp)
-    QString timeString = m_timeStamper->getTimestamp().toUTC().toString("dd-MM-yyyy hh:mm:ss.zzz");
-
-    RecordedEntityComponents newRecord;
-    if(m_currentTimestampRecord.contains(timeString)) {
-        RecordedEntityComponents existingRecord = m_currentTimestampRecord.value(timeString);
-        newRecord = appendToExistingRecord(existingRecord, entityId, componentName, value);
-    }
-    else
-        newRecord = prepareNewRecord(entityId, componentName, value);
-
-    m_periodicTimer->start();
-    m_currentTimestampRecord.insert(timeString, newRecord);
-    m_jsonObject.insert(timeString, convertRecordedEntityComponentsToJson(newRecord));
-
-    if(isRecordComplete(m_currentTimestampRecord.value(timeString))) {
-        m_periodicTimer->stop();
-        m_lastJsonObject = QJsonObject{{timeString, m_jsonObject.value(timeString)}};
-        m_currentTimestampRecord.clear();
-        emit newStoredValue();
-    }
-}
-
-void VeinDataCollector::TimerExpired()
-{
-    m_periodicTimer->stop();
-    if(!m_currentTimestampRecord.isEmpty()) {
-        for(const auto key : m_currentTimestampRecord.keys()) {
-            RecordedEntityComponents newRecord = m_currentTimestampRecord.value(key);
-            m_lastJsonObject = QJsonObject{{key, convertRecordedEntityComponentsToJson(newRecord)}};
-            emit newStoredValue();
-        }
-        m_currentTimestampRecord.clear();
-    }
-}
-
 void VeinDataCollector::prepareTimeRecording()
 {
     m_sigMeasuringCompo = m_storage->getDb()->findComponent(dftEntityId, "SIG_Measuring");
@@ -140,28 +76,6 @@ void VeinDataCollector::prepareTimeRecording()
         qInfo("Graphs recording can't work. RangeModule/SIG_Measuring component is missing.");
 }
 
-RecordedEntityComponents VeinDataCollector::appendToExistingRecord(RecordedEntityComponents existingRecord, int entityId, QString componentName, QVariant value)
-{
-    if(existingRecord.contains(entityId)) { //new component
-        ComponentInfo existingComponents = existingRecord.value(entityId);
-        existingComponents.insert(componentName, value);
-        existingRecord.insert(entityId, existingComponents);
-    }
-    else {//new entity
-        ComponentInfo newComponents {{componentName, value}};
-        existingRecord.insert(entityId, newComponents);
-    }
-
-    return existingRecord;
-}
-
-RecordedEntityComponents VeinDataCollector::prepareNewRecord(int entityId, QString componentName, QVariant value)
-{
-    ComponentInfo newComponents {{componentName, value}};
-    RecordedEntityComponents newRecord {{entityId, newComponents}};
-    return newRecord;
-}
-
 QJsonObject VeinDataCollector::convertRecordedEntityComponentsToJson(RecordedEntityComponents record)
 {
     QJsonObject json;
@@ -171,25 +85,4 @@ QJsonObject VeinDataCollector::convertRecordedEntityComponentsToJson(RecordedEnt
         json.insert(entityIDToString, componentJson);
     }
     return json;
-}
-
-bool VeinDataCollector::isRecordComplete(RecordedEntityComponents record)
-{
-    bool recordComplete = true;
-    for(auto entity: m_targetEntityComponents.keys()) {
-        if(!record.contains(entity))
-            recordComplete = false;
-        else {
-            for(auto componentName: m_targetEntityComponents[entity]) {
-                ComponentInfo recordedComponents = record[entity];
-                if(!recordedComponents.contains(componentName)) {
-                    recordComplete = false;
-                    break;
-                }
-            }
-        }
-        if(!recordComplete)
-            break;
-    }
-    return recordComplete;
 }
