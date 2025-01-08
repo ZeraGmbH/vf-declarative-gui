bb93d197d04e747707e27a1b642f9ba855e42a49
Asmae Kaouissi <a.kaouissi@zera.de>
2025-01-08 17:45:19 +0100

    make singlevaluescaler usable by QML
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- lib/lib-headless/singlevaluescaler.cpp
+++ lib/lib-headless/singlevaluescaler.cpp
@@ -6,6 +6,26 @@ SingleValueScaler::SingleValueScaler(QObject *parent)
 {
 }
 
+bool SingleValueScaler::scaleSingleValForQML(double val)
+{
+    double absVal = fabs(val);
+    TSingleScaleResult singleResult;
+    if(scaleSingleValForPrefix(absVal, 1e15, "P", singleResult))
+        return true;
+    if(scaleSingleValForPrefix(absVal, 1e12, "T", singleResult))
+        return true;
+    if(scaleSingleValForPrefix(absVal, 1e9, "G", singleResult))
+        return true;
+    if(scaleSingleValForPrefix(absVal, 1e6, "M", singleResult))
+        return true;
+    if(scaleSingleValForPrefix(absVal, 1e3, "k", singleResult))
+        return true;
+    if(scaleSingleValForPrefix(absVal, 1e0, "", singleResult))
+        return true;
+    setScale(1e-3, "m", singleResult);
+    return false;
+}
+
 SingleValueScaler::TSingleScaleResult SingleValueScaler::scaleSingleVal(double val)
 {
     double absVal = fabs(val);
@@ -26,11 +46,35 @@ SingleValueScaler::TSingleScaleResult SingleValueScaler::scaleSingleVal(double v
     return singleResult;
 }
 
+void SingleValueScaler::setScaleFactor(double scaleFactor)
+{
+    m_scaleFactor = scaleFactor;
+    emit scaleFactorChanged();
+}
+
+double SingleValueScaler::getScaleFactor()
+{
+    return m_scaleFactor;
+}
+
+void SingleValueScaler::setUnitPrefix(QString unitPrefix)
+{
+    m_unitPrefix = unitPrefix;
+    emit unitPrefixChanged();
+}
+
+QString SingleValueScaler::getUnitPrefix()
+{
+    return m_unitPrefix;
+}
+
 void SingleValueScaler::setScale(double limit, QString limitPrefix, TSingleScaleResult &singleResult)
 {
     singleResult.scaleFactor = 1/limit;
     m_hysteresisValue = limit * HYSTERESIS;
     singleResult.unitPrefix = limitPrefix;
+    setScaleFactor(1/limit);
+    setUnitPrefix(limitPrefix);
 }
 
 bool SingleValueScaler::scaleSingleValForPrefix(double absVal, double limit, QString limitPrefix, TSingleScaleResult &singleResult)
