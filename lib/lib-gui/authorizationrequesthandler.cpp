2056f4c008048885c7a2dcbaa520ef25e1f8b7d3
mdoerr2s <m.doerr@zera.de>
2025-04-29 16:34:21 +0200

    authpopup: first try of accepting request (zeragmbh/websam#1012)
    Signed-off-by: mdoerr2s <m.doerr@zera.de>

--- lib/lib-gui/authorizationrequesthandler.cpp
+++ lib/lib-gui/authorizationrequesthandler.cpp
@@ -1,5 +1,10 @@
 #include "authorizationrequesthandler.h"
 #include <qcryptographichash.h>
+#include <qdebug.h>
+#include <qfileinfo.h>
+#include <qjsonarray.h>
+#include <qjsondocument.h>
+#include <qjsonobject.h>
 
 AuthorizationRequestHandler::AuthorizationRequestHandler(QObject *parent)
     : QObject{parent}
@@ -20,4 +25,51 @@ QString AuthorizationRequestHandler::computeHashString(const QString &input)
         formatted += shortened.mid(i, 2);
     }
     return formatted;
+}
+
+void AuthorizationRequestHandler::finishRequest(const bool &accepted, QVariant requestObject)
+{
+    if(accepted) {
+        appendToJsonFile(m_trustListPath, requestObject.toJsonObject());
+    }
+}
+
+bool AuthorizationRequestHandler::appendToJsonFile(const QString &filePath, const QJsonObject &newObject)
+{
+    // 1. Read existing JSON array
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadWrite)) {
+        qWarning() << "Failed to open file:" << file.errorString();
+        return false;
+    }
+
+    QByteArray jsonData = file.readAll();
+    QJsonParseError parseError;
+    QJsonDocument doc = QJsonDocument::fromJson(jsonData, &parseError);
+
+    if (parseError.error != QJsonParseError::NoError) {
+        qWarning() << "JSON parse error:" << parseError.errorString();
+        return false;
+    }
+
+    // 2. Get or create array
+    QJsonArray array;
+    if (doc.isArray()) {
+        array = doc.array();
+    } else {
+        // Handle case where file contains non-array data
+        qWarning() << "File does not contain a JSON array";
+        return false;
+    }
+
+    // 3. Append new object
+    array.append(newObject);
+
+    // 4. Write back to file
+    file.resize(0); // Clear existing content
+    doc.setArray(array);
+    file.write(doc.toJson(QJsonDocument::Indented));
+    file.close();
+
+    return true;
 }
