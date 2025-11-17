7b19d3d2b230876e3022348d7c5a77ad5bd5f0d4
Andreas Müller <schnitzeltony@gmail.com>
2025-11-17 01:05:59 +0100

    Do not register GraphFunctions.qml - this file does not exist
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- app/main.cpp
+++ app/main.cpp
@@ -101,7 +101,6 @@ static void registerQmlInt()
     qmlRegisterSingletonType(QUrl("qrc:/qml/singletons/SchnubbelState.qml"), "SchnubbelState", 1, 0, "SchnubbState");
     qmlRegisterSingletonType(QUrl("qrc:/qml/singletons/AdjustmentState.qml"), "AdjustmentState", 1, 0, "AdjState");
     qmlRegisterSingletonType(QUrl("qrc:/qml/singletons/PowerModuleVeinGetter.qml"), "PowerModuleVeinGetter", 1, 0, "PwrModVeinGetter");
-    qmlRegisterSingletonType(QUrl("qrc:/qml/singletons/GraphFunctions.qml"), "GraphFunctions", 1, 0, "GraphFunctions");
     qmlRegisterSingletonType(QUrl("qrc:/qml/controls/settings/SlowMachineSettingsHelperSingleton.qml"), "SlowMachineSettingsHelper", 1, 0, "SlwMachSettingsHelper");
     qInfo("Internal QML dependencies registered.");
 }
