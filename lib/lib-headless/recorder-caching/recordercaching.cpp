b6a7698035736b49e4b49c5f83e087117ec5d624
Asmae Kaouissi <a.kaouissi@zera.de>
2025-12-12 10:51:14 +0100

    add recordercaching
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

---
+++ lib/lib-headless/recorder-caching/recordercaching.cpp
@@ -0,0 +1,39 @@
+#include "recordercaching.h"
+#include <QDateTime>
+
+RecorderCaching *RecorderCaching::instance = nullptr;
+
+RecorderCaching::RecorderCaching(QObject *parent)
+    : QObject{parent}
+{}
+
+RecorderCaching *RecorderCaching::getInstance()
+{
+    if(!instance)
+        instance = new RecorderCaching();
+    return instance;
+}
+
+void RecorderCaching::setRecordedValues(QJsonObject newRecordedValues)
+{
+    for (auto it = newRecordedValues.begin(); it != newRecordedValues.end(); ++it)
+        m_recordedObject.insert(it.key(), it.value());
+    emit newValuesRecorded();
+}
+
+QJsonObject RecorderCaching::getRecordedValues()
+{
+    return m_recordedObject;
+}
+
+qint64 RecorderCaching::getFirstTimestamp()
+{
+    QString dateTime = m_recordedObject.keys().isEmpty() ? QString() : m_recordedObject.keys().at(0);
+    return QDateTime::fromString(dateTime, "dd-MM-yyyy hh:mm:ss.zzz").toMSecsSinceEpoch();
+}
+
+void RecorderCaching::clearCashe()
+{
+    m_recordedObject = QJsonObject();
+    emit newValuesRecorded();
+}
